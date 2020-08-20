11273c834527c3cd1d317f23cd2a77f930b0a736
tdausner <thomas@dausner.de>
2020-08-20 22:23:05 +0200

    first commit

---
+++ Resources/Private/Public/JavaScript/main.js
@@ -0,0 +1,52 @@
+;( function ( $, win ) {
+    'use strict';
+
+    /**
+     * menu click & menu scroll handler
+     * @param $target
+     */
+    let scroller = function ( $target ) {
+        window.scrollTo( {
+            top: $target.offset().top - $( 'header .fixed' )[0].getBoundingClientRect().height,
+            left: 0,
+            behavior: 'smooth'
+        } );
+    };
+
+    $( '.menu a' ).click( function ( e ) {
+        e.preventDefault();
+        let id = $( this ).attr( 'href' ).substr( 1 );
+        scroller( $( '[id=' + id + ']' ).parents( 'section' ) );
+    } );
+
+    /**
+     * sounds in Tds.Site:Content.Citation
+     */
+    $( '.citation__icon i' ).click( function () {
+        this.childNodes.length && this.childNodes[0].play();
+    } );
+
+    /**
+     * animation
+     */
+    let setAniatmion = function () {
+        let $elements = $( '.animate' ).parent();
+        $elements.each( function () {
+            let header = $( 'header .fixed' )[0].getBoundingClientRect().height;
+            let rect = this.getBoundingClientRect();
+            let vh = window.innerHeight;
+            let $row = $( this ).find( '.row' );
+            let animation = "animate__animated animate__" + $row.data( 'animation' );
+            if ( rect.top > header && rect.top < vh || rect.bottom > header && rect.top < vh )
+            {
+                $row.addClass( animation );
+            }
+            else
+            {
+                $row.removeClass( animation );
+            }
+        } );
+    };
+    $( win ).on( "resize scroll", setAniatmion );
+
+}( jQuery, window ) );
